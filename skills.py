4aac43d9b13b3103a2787a73ec966c847f1d3919
Katie Byers <lobsterkatie@gmail.com>
2015-09-26 12:15:01 -0700

    advanced-join-strings working

--- skills.py
+++ skills.py
@@ -332,22 +332,44 @@ def average(number_list):
 # Tip: To comment or uncomment blocks of code, highlight what you want to
 #    comment or uncomment, and then CMD+"/" or CTRL-"/"
 
-# def advanced_join_strings(list_of_words):
-#     """Return a single string with each word from the input list
-#     separated by a comma.
+def advanced_join_strings(list_of_words):
+    """Return a single string with each word from the input list
+    separated by a comma.
 
-#         >>> advanced_join_strings(["Labrador", "Poodle", "French Bulldog"])
-#         'Labrador, Poodle, French Bulldog'
+        >>> advanced_join_strings(["Labrador", "Poodle", "French Bulldog"])
+        'Labrador, Poodle, French Bulldog'
 
-#     If there's only one thing in the list, it should return just that
-#     thing, of course:
+    If there's only one thing in the list, it should return just that
+    thing, of course:
 
-#         >>> advanced_join_strings(["Pretzel"])
-#         'Pretzel'
+        >>> advanced_join_strings(["Pretzel"])
+        'Pretzel'
 
-#     """
+    """
 
-#     return ""
+    if len(list_of_words) == 0:
+        return ""
+
+    # same caveat with the 'elif' as the 'else's above
+    elif len(list_of_words) == 1:
+        return list_of_words[0]
+
+    else:
+        return reduce(lambda x, y: x + ", " + y, list_of_words)
+
+        # okay, I don't know if real programmers consider this reduce-with-a-
+        # lambda-function business a party trick, but I like it!
+
+        # in any case, here's the other version:
+
+        # csv_string = list_of_words.pop(0)
+
+        # for word in list_of_words:
+        #     csv_string += ", "
+        #     csv_string += word
+
+        # return csv_string
+
 
 # END OF ASSIGNMENT: You can ignore everything below.
 ##############################################################################
